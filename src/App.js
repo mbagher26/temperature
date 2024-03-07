f7b5d92df88620f4b0c39f98f18bcc56137875e9
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-03-07 11:35:23 -0500

    inport temperature component in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Temperature from './temperature/Temperature';
 
 function App() {
   return (
     <div className="App">
-      
+      <Temperature/>
     </div>
   );
 }
